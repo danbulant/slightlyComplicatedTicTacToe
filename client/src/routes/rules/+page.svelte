2dc1f2c00e32fab049188da0588ac6e20ed21e5f
Neko-Life <nekolife123579@gmail.com>
2023-07-15 16:08:16 +0700

    formatting

--- client/src/routes/rules/+page.svelte
+++ client/src/routes/rules/+page.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-	import BackButton from '$lib/backButton.svelte';
+  import BackButton from '$lib/backButton.svelte';
 </script>
 
 <svelte:head>
-	<title>Ultimate tictactoe</title>
+  <title>Ultimate tictactoe</title>
 </svelte:head>
 
 <BackButton href="/" />
 
 <main class="flex items-center h-screen justify-center flex-col">
-	<div class="container">
-		<h1 class="text-4xl font-bold text-center">Rules</h1>
+  <div class="container">
+    <h1 class="text-4xl font-bold text-center">Rules</h1>
 
-		<div class="content">
-			<p>Content</p>
-		</div>
-	</div>
+    <div class="content">
+      <p>Content</p>
+    </div>
+  </div>
 </main>
 
 <style>
-	.container {
-		@apply w-full;
-		max-width: 1024px;
-	}
+  .container {
+    @apply w-full;
+    max-width: 1024px;
+  }
 
-	.content {
-		font-size: 24px;
-	}
+  .content {
+    font-size: 24px;
+  }
 </style>
